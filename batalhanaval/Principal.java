cfa3be018fce2515a5d1bef2a97f12a3542eaff2
Ricardo Faria <ricardo.faria@outlook.com.br>
2015-10-04 15:13:46 -0300

    Correção na conexão com o servidor

--- batalhanaval/Principal.java
+++ batalhanaval/Principal.java
@@ -5,6 +5,8 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 import Server.Servidor;
 import Server.ServidorImpl;
@@ -14,19 +16,15 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Servidor s = null;
-		try {
-			for (String temp : Naming.list("//127.0.0.1:2050/Server")) {
-				System.out.println(temp);				
-			}
-			System.out.println(Naming.lookup("//127.0.0.1:2050/Server"));
-			//s = (ServidorImpl) Naming.lookup("//127.0.0.1:2050/Server");
-			s = (Servidor) Naming.lookup("//127.0.0.1:2050/Server");
-			
+		try {			
+			Registry registry = LocateRegistry.getRegistry("127.0.0.1", 2050);
+			s = (Servidor) registry.lookup("Server");
+
 			TelaPrincipal principal = new TelaPrincipal(s);
 			principal.pack();
 			principal.setVisible(true);
 
-		} catch (MalformedURLException | RemoteException | NotBoundException e1) {
+		} catch (RemoteException | NotBoundException e1) {
 			e1.printStackTrace();
 		}
 
